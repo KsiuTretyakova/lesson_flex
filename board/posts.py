ef14162d9349cfe978ff7e2dc3e28388ec748757
KsiuTretyakova <hrytsenkoks@gmail.com>
2024-05-11 16:45:51 +0300

    add flash and errors

--- board/posts.py
+++ board/posts.py
@@ -1,12 +1,38 @@
-from flask import Blueprint, render_template
+from flask import (
+    Blueprint,
+    render_template,
+    request,
+    redirect,
+    url_for,
+    flash,
+    current_app
+)
+
+from board.database import get_db
 
 bp = Blueprint("posts", __name__)
 
 @bp.route("/create", methods=("GET", "POST"))
 def create():
+    if request.method == "POST":
+        author = request.form['author'] or "NoName"
+        message = request.form['message']
+        if message:
+            db = get_db()
+            db.execute("INSERT INTO post (author, message) VALUES (?, ?)",(author, message))
+            db.commit()
+            current_app.logger.info(f"New post by {author}")
+            flash(f"Thank you, {author}!", category="success")
+            return redirect(url_for("posts.posts"))
+        else:
+            flash("Something wrong!", category="error")
+
     return render_template("posts/create.html")
+
 
 @bp.route("/posts")
 def posts():
-    posts = []
+    # posts = []
+    db = get_db()
+    posts = db.execute("SELECT author, message, created FROM post ORDER BY created DESC").fetchall()
     return render_template("posts/posts.html", posts=posts)
